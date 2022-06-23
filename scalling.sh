8aac07b8afb83dc246357d8cfa50cceff46ab221
torb235 <74792520+torb235@users.noreply.github.com>
2022-06-23 17:04:27 +0300

    Update scalling.sh

--- scalling.sh
+++ scalling.sh
@@ -1,4 +1,4 @@
 cd /tailscale
-./tailscaled --tun=userspace-networking --socks5-server=localhost:1055 & ./tailscale up --authkey=${TAILSCALE_AUTHKEY}
-ALL_PROXY=socks5://localhost:1055/
+./tailscaled --tun=userspace-networking --outbound-http-proxy-listen=localhost:1055 & ./tailscale up --authkey=${TAILSCALE_AUTHKEY}
+HTTP_PROXY=http://localhost:8055/
 
