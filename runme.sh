6a3e82c0094f27291c776a1ee612d3ab99c05732
torb235 <74792520+torb235@users.noreply.github.com>
2022-06-23 20:15:09 +0300

    Update runme.sh

--- runme.sh
+++ runme.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-{./startup.sh
-./scalling.sh
-cd ~
-./rclone.sh
-qbittorrent-nox --daemon} &> /dev/null
+./startup.sh 
+./scalling.sh &> /dev/null
+./rclone.sh &> /dev/null
+qbittorrent-nox --daemon
