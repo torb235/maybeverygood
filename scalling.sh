3593cb7c7a3f0ecf100f767611f47597022d1669
torb235 <74792520+torb235@users.noreply.github.com>
2022-06-23 19:56:04 +0300

    Update scalling.sh

--- scalling.sh
+++ scalling.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /tailscale
-./tailscaled --tun=userspace-networking --outbound-http-proxy-listen=localhost:1055 & ./tailscale up --authkey=${TAILSCALE_AUTHKEY}&> /dev/null
+./tailscaled --tun=userspace-networking --outbound-http-proxy-listen=localhost:1055 & ./tailscale up --authkey=${TAILSCALE_AUTHKEY} &> /dev/null
 HTTP_PROXY=http://localhost:8055/
