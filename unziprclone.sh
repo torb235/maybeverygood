d771ce5a67ee5834f23ad176518141aa959c4465
torb235 <74792520+torb235@users.noreply.github.com>
2022-06-23 18:31:27 +0300

    Update unziprclone.sh

--- unziprclone.sh
+++ unziprclone.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-unzip -j -d ~ rclone-v1.58.1-linux-amd64 -x "*.txt" "*.html" "*.1" && chmod 777 rclone && ./rclone rcd --rc-web-gui --rc-user=admin --rc-pass=admin &> /dev/null
+./rclone rcd --rc-web-gui --rc-user=admin --rc-pass=admin &> /dev/null
